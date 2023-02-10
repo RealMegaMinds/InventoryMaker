13d3d32a0607119b94a4c530975f7266788c0f32
Hoid2 <61083843+Hoid2@users.noreply.github.com>
2023-02-10 14:00:40 -0500

    Initial Fixes

---
+++ README.md
@@ -0,0 +1,2 @@
+## Inventory Maker
+Allows inventories to be created using commands. Inventories can then be opened using commands and be used to store things.
---
+++ src/main/java/megaminds/inventorymaker/InventoryMaker.java
@@ -0,0 +1,14 @@
+package megaminds.inventorymaker;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InventoryMaker implements ModInitializer {
+	public static final String MODID = "inventorymaker";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
